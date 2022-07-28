257c50d7d09bc7c0cc4301ab732d6ea1401cd5db
Белялов Мемет <memet.belyalov@gazprombank.ru>
2022-07-29 00:14:22 +0300

    init

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { RootStore } from 'dal/root-store';
 
-const BASE_URL = 'http://localhost:3001/';
+const { BASE_URL } = import.meta.env;
 
 export class API {
   rootStore: RootStore;
