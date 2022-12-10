8efef2ddd52a556f3e21c83a261fda6ad0cb4551
Белялов Мемет <memet.belyalov@gazprombank.ru>
2022-12-11 01:12:15 +0300

    update

---
+++ src/components/carousel/style.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import getColor from 'utils/getColor';
+
+export const CarouselPaginationItem = styled.span<{
+  isActive?: boolean;
+}>`
+  width: ${({ isActive }) => (isActive ? '30px' : '16px')};
+  height: 10px;
+  border-radius: 50px;
+  transition: 0.15s all linear;
+  background-color: ${({ isActive }) =>
+    isActive
+      ? getColor('background', 'accent')
+      : getColor('lightGray', 'primary')};
+`;
+
+export const CarouselPaginationWrapper = styled.div`
+  display: flex;
+  column-gap: 10px;
+  width: 100%;
+  justify-content: center;
+  margin-top: 12px;
+`;
+
+export const CarouselItemsWrapper = styled.div`
+  overflow: hidden;
+  position: relative;
+  height: 200px;
+`;
+
+export const CarouselItems = styled.div<{
+  marginLeft: number;
+}>`
+  position: absolute;
+  display: flex;
+  column-gap: 16px;
+  flex-wrap: nowrap;
+  transition: 0.2s all linear;
+  left: ${({ marginLeft }) => `calc(${marginLeft} * -100vw)`};
+`;
+
+export const CarouselItem = styled.span`
+  width: calc(100vw - 8px);
+  padding: 0 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
